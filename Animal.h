3bb88ee8aa4d57ef19839a6c84425e4285c6c9de
khaledmesbah2060-jpg <khaledmesbah2060@gmail.com>
2025-11-12 14:48:39 +0200

    Update Animal.h

--- Animal.h
+++ Animal.h
@@ -1,11 +1,28 @@
 #ifndef ANIMAL_H
 #define ANIMAL_H
 
+#include <iostream>
 #include <string>
 using namespace std;
 
-//define animal class here 
+class Animal {
+private:
+    string name;
+    int age;
+    bool isHungry;
 
-
+public:
+    Animal();
+    Animal(string n, int a, bool h);
+     ~Animal();
+    string getName() ;
+    int getAge() ;
+    bool getIsHungry() ;
+    void setName(string n);
+    void setAge(int a);
+    void setIsHungry(bool h);
+     void display() ;
+    void feed();
+};
 
 #endif
