4647d7624a6d9ffc13180d7ec44decc26e92b01c
khaledmesbah2060-jpg <khaledmesbah2060@gmail.com>
2025-11-12 15:14:39 +0200

    Update Enclosure.h

--- Enclosure.h
+++ Enclosure.h
@@ -1,15 +1,25 @@
-#ifndef ENCLOSURE_H
-#define ENCLOSURE_H
+#ifndef ENCLOSURE_H_INCLUDED
+#define ENCLOSURE_H_INCLUDED
+#include<iostream>
+#include<string>
+#include"Animal.h"
+using namespace std;
 
-#include "Animal.h"
+class Enclosure{
+private:
+    Animal * animal;
+    int capacity;
+    int currentCount;
+public:
+    Enclosure();
+    Enclosure(int c , int cc);
+    ~Enclosure();
+    void addAnimal(Animal *a);
+    void displayAnimals();
 
-//define enclosure class here 
 
 
+};
 
+#endif // ENCLOSURE_H_INCLUDED
 
-
-
-
-
-#endif
